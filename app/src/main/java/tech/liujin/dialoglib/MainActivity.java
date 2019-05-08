6c3fe8a7e32006a9c094371c2e3b595ff9f040c5
liujin <wuxiongc@163.com>
2019-05-08 16:49:18 +0800

    增加系统级显示

--- app/src/main/java/tech/liujin/dialoglib/MainActivity.java
+++ app/src/main/java/tech/liujin/dialoglib/MainActivity.java
@@ -1,10 +1,13 @@
 package tech.liujin.dialoglib;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 import tech.liujin.dialog.fragment.MessageDialog;
 import tech.liujin.dialog.fragment.TransparentDialog;
 import tech.liujin.dialog.window.DialogWindowManager;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
       private Button mWindowBottom;
       private Button mWindowLeft;
       private Button mWindowRight;
+      private Button mCheck;
+      private Button mRequest;
+      private Button mSystemTop;
 
       @Override
       protected void onCreate ( Bundle savedInstanceState ) {
@@ -43,6 +49,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             mWindowLeft.setOnClickListener( this );
             mWindowRight = (Button) findViewById( R.id.windowRight );
             mWindowRight.setOnClickListener( this );
+            mCheck = (Button) findViewById( R.id.check );
+            mCheck.setOnClickListener( this );
+            mRequest = (Button) findViewById( R.id.request );
+            mRequest.setOnClickListener( this );
+            mSystemTop = (Button) findViewById( R.id.systemTop );
+            mSystemTop.setOnClickListener( this );
       }
 
       @Override
@@ -77,8 +89,43 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                         View view2 = getLayoutInflater().inflate( R.layout.message_text, null );
                         DialogWindowManager.showActivityDialogRight( this, view2 );
                         break;
+                  case R.id.check:
+                        boolean overlays = DialogWindowManager.canDrawOverlays( this );
+                        Toast.makeText( this, "是否可以显示" + overlays, Toast.LENGTH_SHORT ).show();
+                        break;
+                  case R.id.request:
+                        DialogWindowManager.requestOverDrawPermission( this, 2048 );
+                        break;
+                  case R.id.systemTop:
+                        View view3 = getLayoutInflater().inflate( R.layout.message_text, null );
+                        try {
+                              DialogWindowManager.showAppDialogTop( this, view3 );
+                        } catch(Exception e) {
+                              e.printStackTrace();
+                        }
+                        break;
                   default:
                         break;
+            }
+      }
+
+      @Override
+      protected void onActivityResult (
+          int requestCode, int resultCode, @Nullable Intent data ) {
+
+            super.onActivityResult( requestCode, resultCode, data );
+
+            if( requestCode == 2048 ) {
+                  if( DialogWindowManager.canDrawOverlays( this ) ) {
+                        View view = getLayoutInflater().inflate( R.layout.message_text, null );
+                        try {
+                              DialogWindowManager.showAppDialogTop( this, view );
+                        } catch(Exception e) {
+                              e.printStackTrace();
+                        }
+                  } else {
+                        Toast.makeText( this, "不能显示系统弹窗", Toast.LENGTH_SHORT ).show();
+                  }
             }
       }
 }
--- dialogManager/src/main/java/tech/liujin/dialog/window/DialogWindowManager.java
+++ dialogManager/src/main/java/tech/liujin/dialog/window/DialogWindowManager.java
@@ -1,7 +1,15 @@
 package tech.liujin.dialog.window;
 
+import android.Manifest.permission;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.provider.Settings;
+import android.support.annotation.RequiresPermission;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,6 +20,8 @@ import tech.liujin.dialog.R;
  * @author Liujin 2019/5/8:10:50:02
  */
 public class DialogWindowManager {
+
+      private static final String TAG = DialogWindowManager.class.getSimpleName();
 
       public static void showActivityDialogTop ( Context context, final View view ) {
 
@@ -67,5 +77,92 @@ public class DialogWindowManager {
                         manager.removeView( view );
                   }
             }, dismissDelayMill );
+      }
+
+      public static boolean canDrawOverlays ( Context context ) {
+
+            if( VERSION.SDK_INT >= VERSION_CODES.M ) {
+                  return Settings.canDrawOverlays( context );
+            }
+
+            return true;
+      }
+
+      public static void requestOverDrawPermission ( Activity activity, int requestCode ) {
+
+            if( VERSION.SDK_INT >= VERSION_CODES.M ) {
+                  activity.startActivityForResult(
+                      new Intent(
+                          Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                          Uri.parse( "package:" + activity.getPackageName() )
+                      ),
+                      requestCode
+                  );
+            }
+      }
+
+      @RequiresPermission(permission.SYSTEM_ALERT_WINDOW)
+      public static void showAppDialog (
+          Context context, final View view, int gravity, int animStyle, int dismissDelayMill ) throws Exception {
+
+            LayoutParams layoutParams = new LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT,
+                0,
+                0,
+                PixelFormat.TRANSPARENT
+            );
+
+            layoutParams.flags = LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |// 不获取焦点，以便于在弹出的时候 下层界面仍然可以进行操作
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR; // 确保你的内容不会被装饰物(如状态栏)掩盖.
+
+            if( VERSION.SDK_INT >= VERSION_CODES.O ) { // 8.0开始需要使用该类型
+                  layoutParams.type = LayoutParams.TYPE_APPLICATION_OVERLAY;
+            } else {
+                  layoutParams.type = LayoutParams.TYPE_PHONE;
+            }
+
+            // 显示位置
+            layoutParams.gravity = gravity;
+            // 动画效果
+            layoutParams.windowAnimations = animStyle;
+
+            final WindowManager manager = (WindowManager) context.getSystemService( Context.WINDOW_SERVICE );
+            manager.addView( view, layoutParams );
+
+            view.postDelayed( new Runnable() {
+
+                  @Override
+                  public void run ( ) {
+
+                        manager.removeView( view );
+                  }
+            }, dismissDelayMill );
+      }
+
+      @RequiresPermission(permission.SYSTEM_ALERT_WINDOW)
+      public static void showAppDialogTop ( Context context, final View view ) throws Exception {
+
+            showAppDialog( context, view, Gravity.TOP, R.style.windowDialogTopAnim, 2000 );
+      }
+
+      @RequiresPermission(permission.SYSTEM_ALERT_WINDOW)
+      public static void showAppDialogBottom ( Context context, final View view ) throws Exception {
+
+            showAppDialog( context, view, Gravity.BOTTOM, R.style.windowDialogBottomAnim, 2000 );
+      }
+
+      @RequiresPermission(permission.SYSTEM_ALERT_WINDOW)
+      public static void showAppDialogLeft ( Context context, final View view ) throws Exception {
+
+            showAppDialog( context, view, Gravity.LEFT | Gravity.TOP, R.style.windowDialogLeftAnim, 2000 );
+      }
+
+      @RequiresPermission(permission.SYSTEM_ALERT_WINDOW)
+      public static void showAppDialogRight ( Context context, final View view ) throws Exception {
+
+            showAppDialog( context, view, Gravity.RIGHT | Gravity.TOP, R.style.windowDialogRightAnim, 2000 );
       }
 }
